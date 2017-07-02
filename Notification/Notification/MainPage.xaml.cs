2ed7f4e1b49925fb9a39f1d80735991ee7724303
klasj\kabzah <Kojan.Abzah@kla-tencor.com>
2017-07-02 18:40:33 +0300

    working example

---
+++ Notification/Notification/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using LocalNotifications.Plugin;
+using LocalNotifications.Plugin.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Notification
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        public void WordNotif(object sender, EventArgs e)
+        {
+            var notification = new LocalNotification
+            {
+                Text = "Hello Plugin",
+                Title = "Nbation Plugin",
+                Id = 2,
+                NotifyTime = DateTime.Now.AddSeconds(2)
+            };
+
+            var notifier = CrossLocalNotifications.CreateLocalNotifier();
+            notifier.Notify(notification);
+        }
+
+    }
+}
